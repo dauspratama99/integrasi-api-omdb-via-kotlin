8b874b66f04d478ff437922242a34b264f85658d
dauspratama99 <dauspratama56@gmail.com>
2022-07-13 10:19:29 +0700

    fix

--- app/src/main/java/com/example/api_omdb/modeldata/MovieData.kt
+++ app/src/main/java/com/example/api_omdb/modeldata/MovieData.kt
@@ -3,8 +3,10 @@ package com.example.api_omdb.modeldata
 import com.google.gson.annotations.SerializedName
 
 data class MovieData (
-        val Year:String, val Title:String,
-        @SerializedName("Poster") val gambar:String
+        val Year:String,
+        val Title:String,
+        @SerializedName("Poster") val gambar:String,
+        @SerializedName("imdbID") val idmovie:String,
 
 
         )
